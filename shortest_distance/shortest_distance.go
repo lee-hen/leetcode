6f0563cd6678e909d08d5a280357731378ec725c
Hen.Lee <lee-hen@users.noreply.github.com>
2021-10-02 15:37:52 +0900

    Add shortest Distance
    Push from ipad

---
+++ shortest_distance/shortest_distance.go
@@ -0,0 +1,36 @@
+func ShortestDistance(wordsDict []string, word1 string, word2 string) int {
+    index1, index2 := -1, -1
+    var distance int
+    for i, str := range wordsDict {
+        if word1 == str {
+            index1 = i
+        } else if word2 == str {
+            index2 = i
+        }
+        
+        if index1 != -1 && index2 != -1 {
+            distance = min(distance, abs(index1-index2))
+        }
+    }
+    return distance
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    
+    return x
+}
+
+func min(a, b int) int {
+    if a == 0 {
+        return b
+    }
+
+    if a < b {
+        return a
+    }
+
+    return b
+}
