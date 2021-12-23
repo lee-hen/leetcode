78962a5f7a70d8fc67ed2c6b05caf17bab3aca04
Hen.Lee <58923415+lee-hen@users.noreply.github.com>
2021-12-23 11:27:55 +0900

    Update num_unique_emails.go

--- unique_email_addresses/num_unique_emails.go
+++ unique_email_addresses/num_unique_emails.go
@@ -5,7 +5,7 @@ import (
 )
 
 func NumUniqueEmails(emails []string) int {
-	domains := make(map[string][]string)
+	domains := make(map[string]map[string]struct{})
 	for i := range emails {
 		address := strings.Split(emails[i], "@")
 		idx := strings.Index(address[0], "+")
@@ -13,10 +13,10 @@ func NumUniqueEmails(emails []string) int {
 			idx = len(address[0])
 		}
 		localName := strings.Replace(address[0][:idx], ".", "", -1)
-		if localNames, ok := domains[address[1]]; ok && localNames[len(localNames)-1] == localName {
-			continue
+		if domains[address[1]] == nil {
+			domains[address[1]] = make(map[string]struct{})
 		}
-		domains[address[1]] = append(domains[address[1]], localName)
+		domains[address[1]][localName] = struct{}{}
 	}
 
 	var count int
