82aa97ddb877b766d634c6e705f594e5078456a6
lee-hen <leehenry303@gmail.com>
2022-05-16 21:52:20 +0900

    Added Binary Tree Vertical Order Traversal rust implementation

--- rust/binary_tree_maximum_path_sum/src/lib.rs
+++ rust/binary_tree_maximum_path_sum/src/lib.rs
@@ -1,25 +1,7 @@
 use std::cell::RefCell;
 use std::cmp;
 use std::rc::Rc;
-
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
+use common::*;
 
 pub fn max_path_sum(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     let mut max_path = i32::MIN;
@@ -55,28 +37,28 @@ mod tests {
 
     #[test]
     fn test_cases() {
-        let root = new_binary_tree(-10);
+        let root = new_tree_node(-10);
         set_left(&root, 9);
         let right = set_right(&root, 20);
         set_left(&right, 15);
         set_right(&right, 7);
         assert_eq!(max_path_sum(root), 42);
 
-        let root = new_binary_tree(1);
+        let root = new_tree_node(1);
         set_left(&root, 2);
         assert_eq!(max_path_sum(root), 3);
 
-        let root = new_binary_tree(-2);
+        let root = new_tree_node(-2);
         set_left(&root, -1);
         set_right(&root, -3);
         assert_eq!(max_path_sum(root), -1);
 
-        let root = new_binary_tree(2);
+        let root = new_tree_node(2);
         set_left(&root, -1);
         set_right(&root, -2);
         assert_eq!(max_path_sum(root), 2);
 
-        let root = new_binary_tree(9);
+        let root = new_tree_node(9);
         set_left(&root, 6);
         let right = set_right(&root, -3);
         set_left(&right, -6);
@@ -86,27 +68,5 @@ mod tests {
         let left = set_left(&left, -6);
         set_left(&left, -6);
         assert_eq!(max_path_sum(root), 16);
-    }
-
-    fn set_left(node: &Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-        let node = Rc::clone(node.as_ref().unwrap());
-        let left_node = Rc::clone(&new_binary_tree(val).unwrap());
-        node.borrow_mut().left = Some(Rc::clone(&left_node));
-        left_node.into()
-    }
-
-    fn set_right(node: &Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-        let node = Rc::clone(node.as_ref().unwrap());
-        let left_node = Rc::clone(&new_binary_tree(val).unwrap());
-        node.borrow_mut().right = Some(Rc::clone(&left_node));
-        left_node.into()
-    }
-
-    fn new_binary_tree(val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-        Some(Rc::new(RefCell::new(TreeNode {
-            val,
-            left: None,
-            right: None,
-        })))
     }
 }
